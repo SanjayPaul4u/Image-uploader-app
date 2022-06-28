feac65cf75cc1974f454d652bd1e24375c75b8ac
SanjayPaul4u <sanjoypaul655@gmail.com>
2022-06-28 09:14:38 +0530

    main work finished: upload a photo

---
+++ image_uploader/server.js
@@ -0,0 +1,23 @@
+const express  = require('express');
+const app = express();
+const path = require('path')
+const port = process.env.PORT || 5000
+const hbs = require('hbs');
+
+const partials_path = path.join(__dirname, "./views_folder/partials")
+
+app.use(express.json());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, "./views_folder"));
+hbs.registerPartials(partials_path)
+
+// serving static file
+app.use(express.static(path.join(__dirname, "./public")));
+
+// route calling
+app.use('/', require('./server/router/router.js'))
+
+
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`);
+})
